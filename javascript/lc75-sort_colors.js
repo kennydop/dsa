8a41f687db3998c86495c8073d3082b268bf1d58
Patrick Danso Offei <dansooffeipatrick@gmail.com>
2022-10-04 08:37:42 +0000

    lc75

--- javascript/lc75-sort_colors.js
+++ javascript/lc75-sort_colors.js
@@ -1,5 +1,25 @@
-var sortColors = function (nums) {};
+var sortColors = function (nums) {
+  let i = 0,
+    j = nums.length - 1;
+  while (nums[i] === 0) i += 1;
+  while (nums[j] === 2) j -= 1;
+  let k = i;
+  while (k <= j) {
+    if (nums[k] === 0) {
+      [nums[i], nums[k]] = [nums[k], nums[i]];
+      i = +1;
+      k -= 1;
+    } else if (nums[k] === 2) {
+      [nums[j], nums[k]] = [nums[k], nums[j]];
+      j -= 1;
+      k -= 1;
+    }
+    k += 1;
+    while (nums[i] === 0) i += 1;
+    while (nums[j] === 2) j -= 1;
+    k = Math.max(k, i);
+  }
+  console.log(nums);
+};
 
-const mergeSort = (arr) => {};
-
-console.log(sortColors([2, 0, 2, 1, 1, 0]));
+sortColors([2, 0, 2, 1, 1, 0]);
